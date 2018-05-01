839c6d0c4bdd486c33e5fba5d45fc992da251ebb
PCDotFan <pc@edu.ax>
2018-05-01 10:21:49 +0800

    2.6 done

--- 2.6/src/index.js
+++ 2.6/src/index.js
@@ -2,6 +2,7 @@ import { san, DataTypes } from "san";
 import { router, Link } from "san-router";
 
 import TwoPointSix from "./components/TwoPointSix.san";
+import UIkit from 'uikit';
 
 router.add({
   rule: "/",
